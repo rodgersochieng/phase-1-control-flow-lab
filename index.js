02c79f86e8cd76dbf7273e0545bcf93da51ab288
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-08-16 22:18:41 +0300

    this didnt work out quite well

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  scuberGreetingForFeet(2501);
+  if(ride <= 400 ){
+  return `This one is on me!`}
+
+   else if(ride > 2000 && ride <= 2500){
+  return `I will gladly take your thirty bucks.`}
+    else if(ride > 2500 )
+  {
+  return `No can do`}
+ 
+
+  }
+  
+
+
+function ternaryCheckCity(City){
+  
+  const destination = City === 'NYC' ? 'ok,sounds good' :'No,go'
+  return destination;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous' 
+    : return 'Thank you so much'
+    case 'not as generous'
+     : return 'Thank you'
+    default 
+    :return 'Bye'
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
