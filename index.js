52d66abd9b20f4e7c1c57e0587b22437d1c0a1ec
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-08-18 19:37:46 +0300

    comleted lab

--- index.js
+++ index.js
@@ -1,33 +1,27 @@
-function scuberGreetingForFeet(ride){
-  scuberGreetingForFeet(2501);
-  if(ride <= 400 ){
-  return `This one is on me!`}
-
-   else if(ride > 2000 && ride <= 2500){
-  return `I will gladly take your thirty bucks.`}
-    else if(ride > 2500 )
-  {
-  return `No can do`}
- 
-
+function scuberGreetingForFeet(someValue){
+  let result;
+  if(someValue <= 400){
+    result = 'This one is on me!';
+    return result;
+  }else if((someValue > 2000) && (someValue < 2500)) {
+    result = 'I will gladly take your thirty bucks.';
+    return result;
+  }else {
+    return 'No can do.';
   }
-  
-
-
-function ternaryCheckCity(City){
-  
-  const destination = City === 'NYC' ? 'ok,sounds good' :'No,go'
-  return destination;
 }
+function ternaryCheckCity(city){
+  // Write your code here!
+  let result = city === 'NYC' ? "Ok, sounds good." : "No go.";
+  return result;
+}
+function switchOnCharmFromTip(tip) {
 
-function switchOnCharmFromTip(tip){
-  switch(tip){
-    case 'generous' 
-    : return 'Thank you so much'
-    case 'not as generous'
-     : return 'Thank you'
-    default 
-    :return 'Bye'
+  if (tip === "generous") {
+      return "Thank you so much.";
+  } else if (tip === "not as generous") {
+      return "Thank you.";
+  } else {
+      return "Bye.";
   }
-
 }
